cc7591c233d54c8ba20e4d1d5b3742954353b19e
guizinsx <guizin51@gmail.com>
2025-08-22 11:38:05 -0300

    Feat: adicionar campos de formulario e titulo da janela com logo

--- tela.py
+++ tela.py
@@ -48,5 +48,36 @@ frame_details.grid(row=1, column=1, pady=1, padx=10, sticky=NSEW)
 frame_tabela = Frame(janela, width=800, height=100, bg=co1, relief=SOLID)
 frame_tabela.grid(row=3, column=0, pady=0, padx=10, sticky=NSEW, columnspan=5)
 
+# -------- Trabalhando no frame logo -------
+global imagem, imagem_string, l_imagem
+
+app_lg = Image.open("logo.png")
+app_lg = app_lg.resize((50,50))
+app_lg = ImageTk.PhotoImage(app_lg)
+app_logo = Label(frame_logo, image=app_lg, text="  Sistema de Registro de Alunos", width=850, compound=LEFT, anchor=NW, font=('Verdana 15'), bg=co6, fg=co1)
+app_logo.place(x=5, y=0)
+
+# ---- Criando os campos de entrada -----
+l_nome = Label(frame_details, text="Nome: ", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_nome.place(x=4, y=10)
+e_nome = Entry(frame_details, width=30, justify='left', relief='solid')
+e_nome.place(x=7, y=40)
+
+l_email = Label(frame_details, text="Email: ", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_email.place(x=4, y=70)
+e_email = Entry(frame_details, width=30, justify='left', relief='solid')
+e_email.place(x=7, y=100)
+
+l_telefone = Label(frame_details, text="Telefone: ", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_telefone.place(x=4, y=130)
+e_telefone = Entry(frame_details, width=15, justify='left', relief='solid')
+e_telefone.place(x=7, y=160)
+
+l_sexo = Label(frame_details, text="Sexo: ", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_sexo.place(x=127, y=130)
+c_sexo = ttk.Combobox(frame_details, width=7, font=('Ivy 8 bold'), justify='center')
+c_sexo['values'] = ('M', 'F', 'Outro')
+c_sexo.place(x=130, y=160)
+
 
 janela.mainloop()
